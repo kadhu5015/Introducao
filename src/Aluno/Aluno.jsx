2e1aba4530ae1fcdb371e356f0d5165ff54e77a9
kadhu5015 <eduardoalmeida8246@gmail.com>
2021-09-28 20:48:30 -0300

    primeiro commit

---
+++ src/Aluno/Aluno.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from 'react-native'
+
+export default function hello(){
+    return(
+        <View>
+            <Text>Carlos Eduardo Almeida Feitosa</Text>
+            <Text>18 anos</Text>
+            <Text>Quixadá</Text>
+            <Text>Design Digital</Text>
+
+        </View>
+    )
+}
